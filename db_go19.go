ee135400fe22add2ce148f31472201600d3681e0
Kyril Revels <kyril.revels@rakuten.com>
2019-05-31 16:05:48 -0400

    supports usage of out params during transactions

---
+++ db_go19.go
@@ -0,0 +1,19 @@
+// +build go1.9
+
+package txdb
+
+import (
+	"database/sql"
+	"database/sql/driver"
+)
+
+
+// Implement the NamedValueChecker interface
+func (c *conn) CheckNamedValue(nv *driver.NamedValue) error {
+	switch nv.Value.(type) {
+	case sql.Out:
+		return nil
+	default:
+		return driver.ErrSkip
+	}
+}
